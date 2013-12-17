1cdac74c7d01177914085137e9876f78422f0b37
Gabriel Ratener <gabe9693@gmail.com>
2013-12-17 00:36:50 -0500

    worked on and fixed board GUI constructor

--- public/js/board.js
+++ public/js/board.js
@@ -1,17 +1,27 @@
 function Board(container, params){
-	var thiss = this;
 
 	this.__container = container;
 	this.__params = params;
 
+
+	var board = [];
+	for(var i = 0; i < params.size[0]; i++){
+		board.push([]);
+		for(var j = 0; j < params.size[1]; j++){
+			board[i].push(false);
+		}
+	}
+	this.__board = board;
+
 	var clas = (container.getAttribute("class") || "") + " board_container";
 	container.setAttribute("class", clas);
 
-	var canvas = document.createElement("canvas");
-	container.appendChild(canvas);
 
 	var board = document.createElement("canvas");
 	container.appendChild(board);
+
+	var canvas = document.createElement("canvas");
+	container.appendChild(canvas);
 
 	this.__back = {
 		"context": false,
@@ -26,16 +36,16 @@ function Board(container, params){
 	this.__spaces = params.size;
 
 	canvas.onmousemove = function(e){
-		var rect = this.getBoundingClientRect();
-		thiss.over({
+		var rect = canvas.getBoundingClientRect();
+		thiss.__over({
 			"x": e.clientX - rect.left,
 			"y": e.clientY - rect.top
 		});
 	}
 
 	canvas.onclick = function(e){
-		var rect = this.getBoundingClientRect();
-		thiss.clicked({
+		var rect = canvas.getBoundingClientRect();
+		thiss.__clicked({
 			"x": e.clientX - rect.left,
 			"y": e.clientY - rect.top
 		});
@@ -43,18 +53,97 @@ function Board(container, params){
 
 	this.__last = false;
 
+	this.ontilehover = function(){};
+	this.ontileclick = function(){};
+
+	var thiss = this;
+
 	this.resetCanvas();
 }
 
 Board.prototype = (function(){
 	var me = {};
 
-	me.over = function(){
-		
+	me.__clear = function(pt){
+		if(pt) {
+			var w = this.__dimensions[0] / this.__spaces[0],
+				h = this.__dimensions[1] / this.__spaces[1],
+				x0 = pt[0] * w,
+				y0 = pt[1] * h,
+				xf = (pt[0] + 1) * w,
+				yf = (pt[1] + 1) * h;
+
+			this.__front.context.clearRect(x0, y0, xf, yf);
+		}else return false;
 	}
 
-	me.clicked = function(){
+	me.__over = function(ev){
+		var dims = this.__dimensions;
 
+		var x = Math.floor(this.__spaces[0] * ev.x / dims[0]),
+			y = Math.floor(this.__spaces[1] * ev.y / dims[1]);
+
+		if(x >= dims[0]) x = dims[0] - 1;
+		if(y >= dims[0]) y = dims[0] - 1;
+
+		if(x < 0) x = 0;
+		if(y < 0) y = 0;
+
+		var now = [x, y],
+			las = this.__last;
+		if(now !== las){
+			// todo: clear last
+			if(!!las && !this.__board[las[0]][las[1]]){
+				this.__clear(this.__last);				
+			}
+
+			this.__last = now;
+			this.ontilehover(now);
+		}
+	}
+
+	me.__clicked = function(ev){
+		var dims = this.__dimensions;
+
+		var x = Math.floor(this.__spaces[0] * ev.x / dims[0]),
+			y = Math.floor(this.__spaces[1] * ev.y / dims[1]);
+
+		if(x >= dims[0]) x = dims[0] - 1;
+		if(y >= dims[0]) y = dims[0] - 1;
+
+		if(x < 0) x = 0;
+		if(y < 0) y = 0;
+
+		this.ontileclick([
+			x,
+			y
+		]);
+	}
+
+	me.__circle = function(pt, color){
+		var w = this.__dimensions[0] / this.__spaces[0],
+			h = this.__dimensions[1] / this.__spaces[1],
+			x = (pt[0] + 0.5) * w,
+			y = (pt[1] + 0.5) * h,
+			r = 0.45 * Math.min(w, h),
+			cc = this.__front.context;
+
+		cc.fillStyle = color;
+		cc.beginPath();
+		cc.arc(x, y, r, 0, 2 * Math.PI);
+		cc.closePath();
+		cc.fill();
+	}
+
+	me.putStone = function(pt, color){
+		var bow = (!!color) ? "#FFF" : "#000";
+		this.__board[pt[0]][pt[1]] = true;
+		this.__circle(pt, bow);
+	}
+
+	me.putGhost = function(pt, color){
+		var bow = (!!color) ? "rgba(255,255,255, 0.5)" : "rgba(0,0,0,0.5)";
+		this.__circle(pt, bow);
 	}
 
 	me.resetCanvas = function(){
